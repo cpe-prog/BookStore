bc3a9d43a9b6fb8edc32f5afa1e0945c99d0381f
Grian Gajila <griangajila2@gmail.com>
2023-04-21 11:11:31 +0800

    using table from spectre.console for rendering books format

--- Program.cs
+++ Program.cs
@@ -1,3 +1,80 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using Newtonsoft.Json;
+using Spectre.Console;
+using static Spectre.Console.AnsiConsole;
+using Console = System.Console;
 
-Console.WriteLine("Hello, World!");
+namespace BookStore;
+
+internal abstract class Program
+{
+    [Obsolete("Obsolete")]
+    private static void Main(string[] args)
+    {
+        if (args.Length == 0)
+        {
+            return;
+        }
+
+        switch (args[0])
+        {
+            case "add":
+                if (args.Length < 2 || args[1] == "books")
+                {
+                    AddBooks();
+                }
+                break;
+            default:
+                Console.WriteLine("Invalid command.");
+                break;
+        }
+    }
+
+    [Obsolete("Obsolete")]
+    private static void AddBooks()
+    {
+        var books = new List<Book>();
+
+        while (true)
+        {
+            var book = new Book();
+
+            Console.Write("Title: ");
+            book.Title = Console.ReadLine();
+
+            Console.Write("Author: ");
+            book.Author = Console.ReadLine();
+
+            Console.Write("ID: ");
+            if (!int.TryParse(Console.ReadLine(), out var id))
+            {
+                Console.WriteLine("Invalid ID. Please enter a number.");
+                continue;
+            }
+            book.Id = id;
+
+            books.Add(book);
+
+            Console.Write("Add another book? (y/n): ");
+            if (Console.ReadLine()!.ToLower() != "y")
+            {
+                break;
+            }
+        }
+
+        var json = JsonConvert.SerializeObject(books, Formatting.Indented);
+        var path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Library.json");
+        File.WriteAllText(path, json);
+
+        var table = new Table();
+        table.AddColumn("ID");
+        table.AddColumn("Title");
+        table.AddColumn("Author");
+
+        foreach (var book in books)
+        {
+            table.AddRow(book.Id.ToString(), book.Title!, book.Author!);
+        }
+
+        Render(table);
+    }
+}
